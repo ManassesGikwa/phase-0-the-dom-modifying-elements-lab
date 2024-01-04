998d6da148590f64771ec6b17f8f6763a1a9a96f
ManassesGikwa <manassesgikwa@gmail.com>
2024-01-04 15:24:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+// Create and modify 'newHeader' element
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Manasses is the champion"; 
+
+document.body.appendChild(newHeader);
